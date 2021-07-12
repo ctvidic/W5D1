1d8186b469fda4d93c7a7bc2ba09e28502060e40
Sunny Mei <sunmeiappprep@gmail.com>
2021-07-12 13:31:44 -0400

    resize'

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -1,14 +1,28 @@
 class MaxIntSet
   def initialize(max)
+      @store = Array.new(max)
+  end
+
+  def store
+    @store
+  end
+
+  def store=(num)
+    @store[num]
   end
 
   def insert(num)
+    raise 'Out of bounds' if num > @store.length-1 || num < 0
+    @store[num] = true
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    return true if @store[num]
+    false
   end
 
   private
@@ -27,17 +41,22 @@ class IntSet
   end
 
   def insert(num)
+    self[num].push(num)
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    return true if self[num].include?(num)
+    false
   end
 
   private
 
   def [](num)
+    @store[num % num_buckets]
     # optional but useful; return the bucket corresponding to `num`
   end
 
@@ -54,19 +73,32 @@ class ResizingIntSet
     @count = 0
   end
 
-  def insert(num)
+  def insert(num)    
+    if count == num_buckets
+      resize!
+    end
+    if !self.include?(num)
+      self[num].push(num)
+      @count += 1
+    end
   end
 
   def remove(num)
+    if self.include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    return true if self[num].include?(num)
+    false
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -74,5 +106,18 @@ class ResizingIntSet
   end
 
   def resize!
+    # @store <<  Array.new(num_buckets)
+    if @count > num_buckets
+      flat = @store.flatten
+      # num_buckets *= 2
+      double = num_buckets*2
+      @store = Array.new(double) { Array.new }
+      # @count = 0
+      flat.each do |ele|
+        insert(ele)
+      end
+    end
+  
+    return @store.length
   end
 end
